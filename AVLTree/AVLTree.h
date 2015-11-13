c5a41e41d54f725105101a8fc0e8d5439b61acea
Right2Drive <bjpfannm@ucalgary.ca>
2015-11-13 11:20:39 -0700

    Added a folder for the AVL Tree files. \nAlso added the two files making up the AVL tree to the folder.

---
+++ AVLTree/AVLTree.h
@@ -0,0 +1,224 @@
+#pragma once
+
+#include "Node.h"
+#include <assert.h>
+
+
+
+using namespace std;
+/*
+	Implementation created with assistance from http://kukuruku.co/hub/cpp/avl-trees
+
+REQUIRES:
+	T must implement a copy constructor and overload < and > in order to call the functions
+*/
+template <class T> class AVLTree
+{
+private:
+	// Fields
+	Node<T>* root;
+
+	// Operations
+	Node<T>* nodeInsert(Node<T>*&, T);
+	Node<T>* findMin(Node<T>*);
+	Node<T>* removeMin(Node<T>*&);
+	void destruct(Node<T>*&);
+	Node<T>* deleteHelper(Node<T>*&, T);
+	Node<T>* helper(Node<T>*);
+public:
+	//////// Operations ///////////
+	/*
+	PROMISES:
+		Creates an AVL tree with a null root
+	*/
+	AVLTree();
+	/*
+	PROMISES:
+		will insert a Node into the AVL tree with a key of type T, and then rebalance the tree
+	REQUIRES:
+		
+	*/
+	Node<T>* insert(T);
+	/*
+	PROMISES:
+		Will remove the left-most node from the tree, rebalance it, and return the value of the
+		removed node to the user
+	REQUIRES:
+		The tree must contain nodes, will merely return NULL if called upon an empty tree
+	*/
+	T* popMin();
+	void deleteNode(T);
+	/*
+	PROMISES:
+		will return the value of the left-most node
+	*/
+	T* readMin();
+	/*
+	PROMISES:
+		will delete all memory being used by the tree;
+	*/
+	~AVLTree();
+};
+
+template <class T>
+AVLTree<T>::AVLTree()
+	: root(nullptr)
+{
+
+}
+
+template <class T>
+Node<T>* AVLTree<T>::insert(T newItem) {
+#ifdef DEBUG
+	cout << "Starting insert operator...\n";
+#endif
+	return nodeInsert(root, newItem);
+}
+
+
+template <class T>
+Node<T>* AVLTree<T>::nodeInsert(Node<T>* &head, T newItem) {
+	if (!head) {
+			head = new Node<T>(newItem);
+	}
+	else
+	{
+		if (newItem < head->itemM) {
+			head->left = nodeInsert(head->left, newItem);
+		}
+		else {
+			head->right = nodeInsert(head->right, newItem);
+		}
+	}
+	head = head->balance();
+	return head;
+}
+
+template <class T>
+Node<T>* AVLTree<T>::findMin(Node<T>* node)
+{
+	if (!(node->left))
+		return node;
+	return node->left ? findMin(node->left) : node;
+}
+
+template <class T>
+T* AVLTree<T>::readMin() {
+	assert(root);
+	return &((findMin(root))->itemM);
+}
+
+
+template<class T>
+T* AVLTree<T>::popMin()
+{
+	if (!root)
+		return NULL;
+	Node<T>* destroy = removeMin(root);
+	T target = destroy->itemM;
+	delete destroy;
+	destroy = NULL;
+	if (root) {
+		root = root->balance();
+	}
+	return &target;
+}
+
+template<class T>
+void AVLTree<T>::deleteNode(T target)
+{
+	deleteHelper(root, target);
+}
+
+template<class T>
+Node<T>* AVLTree<T>::deleteHelper(Node<T>*& head, T target)
+{
+	if (!head) {
+		return NULL;
+	}
+	if (target < head->itemM)
+		head->left = deleteHelper(head->left, target);
+	else if (target > head->itemM)
+		head->right = deleteHelper(head->right, target);
+	else 
+	{
+		Node<T>* l = head->left;
+		Node<T>* r = head->right;
+		delete head;
+		if (!r) {
+			return l;
+		}
+		else
+		{
+			Node<T>* min = findMin(r);
+			min->right = helper(r);
+			min->left = l;
+			head = min->balance();
+		}
+	}
+	head = head->balance();
+	return head;
+}
+
+template <class T>
+Node<T>* AVLTree<T>::helper(Node<T>* p) {
+	if (p->left == 0)
+		return p->right;
+	p->left = helper(p->left);
+	return p->balance();
+}
+
+
+template <class T>
+Node<T>* AVLTree<T>::removeMin(Node<T>* &head)
+{
+	Node<T>* target;
+	if (!head) {	// Exceptional case (will only run on first run)
+		return NULL;
+	}
+	if (!head->left) {	// Exceptional case (will only run on first run)
+		target = head;
+		if (!head->right) { // Head is only node
+			head = NULL;
+			return target;
+		}
+		// There are nodes to the right of head
+		target = head;
+		head = head->right; // Set the right of head as new head
+		return target;
+	}
+	if (!(head->left->left)) { // Base case
+		target = head->left;
+		head->left = head->left->right;
+		head = head->balance();
+		return target;
+	}
+	return removeMin(head->left); // Move further down the left side of the tree
+}
+
+
+
+template<class T>
+AVLTree<T>::~AVLTree()
+{
+	destruct(root);
+	if (root)
+		cout << "Tree was not properly destroyed\n";
+}
+
+template <class T>
+void AVLTree<T>::destruct(Node<T>* &node) {
+	if (!node) { // Exceptional case where the tree is empty to begin with
+		return;
+	}
+	if (node->right)
+		destruct(node->right);
+	if (node->left)
+		destruct(node->left);
+	delete node;
+	node = NULL;
+}
+
+
+
+
---
+++ AVLTree/Node.h
@@ -0,0 +1,137 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template <class S> class Node	{
+	template <class T> friend class AVLTree;
+public:
+	Node();
+	Node(const Node&);
+	Node(S key);
+	S getItem();
+	~Node();
+private:
+	// Fields 
+	S itemM;
+	int height;
+	Node<S>* left;
+	Node<S>* right;
+
+	// Operations
+	int calcBalance();
+	void repair();
+	Node<S>* balance();
+	Node<S>* rotateRight();
+	Node<S>* rotateLeft();
+
+};
+
+template <class S>
+Node<S>::Node()
+	: itemM(0), left(nullptr), right(nullptr), height(-1)
+{
+
+}
+
+template<class S>
+Node<S>::Node(const Node<S> &src)
+	: left(src.left), right(src.right), itemM(src.itemM), height(src.height)
+{
+}
+
+
+
+template<class S>
+Node<S>::Node(S key)
+	: itemM(key), left(nullptr), right(nullptr), height(-1)
+{
+}
+
+template <class S>
+S Node<S>::getItem() {
+	return itemM;
+}
+
+template <class S>
+Node<S>* Node<S>::balance() {
+	repair();
+	if (calcBalance() == 2) {
+		if (right->calcBalance() < 0)
+			right = right->rotateRight();
+		return rotateLeft();
+	}
+	if (calcBalance() == -2) {
+		if (left->calcBalance() > 0)
+			left = left->rotateLeft();
+		return rotateRight();
+	}
+	return this;
+}
+
+template<class S>
+Node<S>* Node<S>::rotateRight()
+{
+	Node<S>* temp = left;
+	left = temp->right;
+	temp->right = this;
+	repair();
+	temp->repair();
+	return temp;
+}
+
+template<class S>
+Node<S>* Node<S>::rotateLeft()
+{
+	Node<S>* temp = right;
+	right = temp->left;
+	temp->left = this;
+	repair();
+	temp->repair();
+	return temp;
+}
+
+template<class S>
+int Node<S>::calcBalance()
+{
+	if (!this) {
+		return 0;
+	}
+	else if (!left || !right) {
+		if (!left && !right)
+			return 0;
+		else if (!left)
+			return right->height;
+		else
+			return -left->height;
+	}
+	else
+		return right->height - left->height;
+}
+
+template<class S>
+void Node<S>::repair()
+{
+	// Check for null values:
+	if (!left || !right) {
+		if (!left && !right)
+			height = 1;
+		else if (!left)
+			height = right->height + 1;
+		else
+			height = left->height + 1;
+	}
+	else
+	{
+		// Take the difference
+		int h1 = left->height;
+		int h2 = right->height;
+		height = (h1 > h2 ? h1 : h2) + 1;
+	}
+}
+
+template <class S>
+Node<S>::~Node()
+{
+
+}
